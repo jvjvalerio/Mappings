0b735cca99fa8f6d5a07187d3a3c84ea317e3f9d
this.julio <julio.valerio2@live.lagcc.cuny.edu>
2022-10-18 15:24:05 -0400

    Upload Fallback Task

--- YourContract.sol
+++ YourContract.sol
@@ -118,4 +118,14 @@ contract YourContract is ERC20 {
     function emitProfileUpdated(address _user) external {
         emit ProfileUpdated(_user);
     }
+
+    // <!--FALLBACK TASK-->
+    /* To ensure that our smart contract can receive ETH sent to it via transfers, 
+    we will create a fallback or receive payable function. 
+    The task is to create the fallback function and make sure when a user transfers 
+    ETH to the smart contract, the transaction does not get reverted */
+
+    fallback() external payable {}
+
+    receive() external payable {}
 }
